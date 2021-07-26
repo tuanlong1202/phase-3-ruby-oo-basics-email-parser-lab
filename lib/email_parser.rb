ac14ea81bf9ba264e50fca6cbd84a022f8db253c
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-26 18:57:06 -0400

    Phase 3 ruby oo basics email parser lab

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,28 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+    attr_accessor :email_addresses
+    def initialize(emails)
+        @email_addresses = emails
+    end
+    def email?(email)
+        arrNameHost = []
+        arrHost = []
+        arrNameHost = email.split("@")
+        if arrNameHost.count == 2
+            arrHost = arrNameHost[1].split(".")
+        end
+        return (arrNameHost.count == 2) && (arrHost.count == 2)
+    end
+    def parse
+        arrRet = []
+        arrEmails = @email_addresses.split(/[\s,]/)
+        arrEmails.each do |item|
+            if email?(item) && !arrRet.include?(item)
+                arrRet << item
+            end
+        end
+        arrRet
+    end
+end
